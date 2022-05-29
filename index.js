7031ac38d3e9c76106ac183aabd90f03eafbf5a0
danielchristoper <christoperdaniel0412@gmail.com>
2022-05-30 00:07:33 +0700

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-//import cors from "cors";
+import cors from "cors";
 import db from "./config/Database.js";
 import router from "./routes/index.js";
 //import Users from "./models/UserModel.js";
@@ -17,7 +17,7 @@ try {
 
 }
 
-//app.use(cors({credentials:true, origin: 'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
